0d23f6a02cd3b18d1631694b9bff273fc7ce1724
Masterdiekillsu <masterdiekillsu@gmail.com>
2017-12-23 19:35:25 -0800

    Stat design has been updated. Stat mods now change

--- myJava.js
+++ myJava.js
@@ -1,4 +1,53 @@
-window.onload = function()
-	{
-		alert("hey, this works");
+(function () {
+	window.onload=function(){
+		document.getElementById("str").onchange = strChange;
+		document.getElementById("perc").onchange = percChange;
+		document.getElementById("fort").onchange = fortChnge;
+		document.getElementById("char").onchange = charChange;
+		document.getElementById("intel").onchange = intelChange;
+		document.getElementById("dex").onchange = dexChange;
+		document.getElementById("luck").onchange = luckChange;
 	}
+	
+	function strChange() {
+		var stat = document.getElementById("str").value;
+		var strMod = (stat - 5) * 4;
+		document.getElementById("strMod").value = strMod;
+	}
+	
+	function percChange() {
+		var stat = document.getElementById("perc").value;
+		var strMod = (stat - 5) * 4;
+		document.getElementById("percMod").value = strMod;
+	}
+		
+	function fortChange() {
+		var stat = document.getElementById("fort").value;
+		var strMod = (stat - 5) * 4;
+		document.getElementById("fortMod").value = strMod;
+	}
+		
+	function charChange() {
+		var stat = document.getElementById("char").value;
+		var strMod = (stat - 5) * 4;
+		document.getElementById("charMod").value = strMod;
+	}
+
+	function intelChange() {
+		var stat = document.getElementById("intel").value;
+		var strMod = (stat - 5) * 4;
+		document.getElementById("intelMod").value = strMod;
+	}
+
+	function dexChange() {
+		var stat = document.getElementById("dex").value;
+		var strMod = (stat - 5) * 4;
+		document.getElementById("dexMod").value = strMod;
+	}
+
+	function luckChange() {
+		var stat = document.getElementById("luck").value;
+		var strMod = (stat - 5) * 4;
+		document.getElementById("luckMod").value = strMod;	
+	}
+})()
